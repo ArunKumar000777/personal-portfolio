84a8887bad33dfda07a7710045c853366a23da1b
ArunKumar000777 <arunashok1020@gmail.com>
2024-02-28 21:54:04 +0530

    add header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,21 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById("nav-menu");
+navToggle = document.getElementById("nav-toggle");
+navClose = document.getElementById("nav-close");
+// ============MENU SHOW ============
+// Validate if constants exist
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+// ============MENU HIDDEN ============
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
-
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
